474c72c76fb15aea6171682e5ca241819273b142
Gwyn <gwyn.barrientos@kcl.ac.uk>
2022-10-24 15:58:00 +0100

    prime function

--- primes.py
+++ primes.py
@@ -1,6 +1,30 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(num):
+    x = int(num)
+    if x ==1:
+        break
+    elif x==2:
+        return 1
+    for i in range(2:round(x/2)+1):
+        if x%i==0:
+            break
+        elif x % i !=0:
+            if i == round(x/2):
+                return 1
+
+
+
+
 def primes(number_of_primes):
     list = []
+    n =int(number_of_primes)
+    y = 0
+    prime_counter = 0
+    while prime_counter<=n-1:
+        if isPrime(y) ==1:
+            list.append(y)
+            prime_counter +=1
+        y +=1
     return list
